7ae47ee155f6517c09860d6fd2f3da6c8f88862b
ross <ross@892a43ee-c559-4cf5-acd1-8b7d2544a145>
2011-05-26 00:24:55 +0000

    added setup.py to work towards building with distutils

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="Templeton",
+    packages=['templeton', 'templeton.design', 'templeton.extract', 'templeton.draw', 'templeton.resources'],
+    version="0.1",
+    url="http://rossfenning.co.uk/",
+    author="Ross Fenning",
+    author_email="ross.fenning@gmail.com",
+    package_dir = {'': 'src/main'},
+    )
+
