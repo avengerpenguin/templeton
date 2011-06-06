bc405d5cb463d97a23cb43d0a8bfd3bbcd03d08a
ross <ross@892a43ee-c559-4cf5-acd1-8b7d2544a145>
2011-06-06 23:53:44 +0000

    Removed debug echos

--- rut.py
+++ rut.py
@@ -1,13 +1,11 @@
 import sys
 
-sys.path.append('/home/ross/workspace/elk/src/main/')
-
 import elk
 from elk import core, python
 
 params = dict(
               name="templeton", 
-              version="0.0.2", 
+              version="0.0.3", 
               url="http://rossfenning.co.uk/", 
               author="Ross Fenning", 
               author_email="ross.fenning@gmail.com",
@@ -17,5 +15,4 @@ params = dict(
     )
 
 if __name__ == "__main__":
-    print "foo"
     elk.main(params)
