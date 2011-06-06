005a62268d0bbb7473a1703ba1a8de94e4c90415
ross <ross@892a43ee-c559-4cf5-acd1-8b7d2544a145>
2011-06-06 19:05:54 +0000

    Hopefully now a working rut script for building with elk.

--- projects/lib/coa/templeton/trunk/rut.py
+++ projects/lib/coa/templeton/trunk/rut.py
@@ -3,15 +3,16 @@ import sys
 sys.path.append('/home/ross/workspace/elk/src/main/')
 
 import elk
-from elk import core
-from elk import python
+from elk import core, python
 
 params = dict(
               name="templeton", 
               version="0.0.1", 
               url="http://rossfenning.co.uk/", 
               author="Ross Fenning", 
-              author_email="ross.fenning@gmail.com", 
+              author_email="ross.fenning@gmail.com",
+              packages=['templeton', 'templeton.design', 'templeton.extract',
+                        'templeton.draw', 'templeton.resources'],
     )
 
 if __name__ == "__main__":
