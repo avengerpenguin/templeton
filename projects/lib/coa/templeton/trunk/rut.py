00e4b5ae7f0dfe0f80e14c6d4e323e656e1b0c6b
ross <ross@892a43ee-c559-4cf5-acd1-8b7d2544a145>
2011-06-06 23:37:32 +0000

    Updated dependencies.

--- projects/lib/coa/templeton/trunk/rut.py
+++ projects/lib/coa/templeton/trunk/rut.py
@@ -7,13 +7,15 @@ from elk import core, python
 
 params = dict(
               name="templeton", 
-              version="0.0.1", 
+              version="0.0.2", 
               url="http://rossfenning.co.uk/", 
               author="Ross Fenning", 
               author_email="ross.fenning@gmail.com",
               packages=['templeton', 'templeton.design', 'templeton.extract',
                         'templeton.draw', 'templeton.resources'],
+              depends=['python-mock', 'python-unittest', 'python-imaging']
     )
 
 if __name__ == "__main__":
+    print "foo"
     elk.main(params)
