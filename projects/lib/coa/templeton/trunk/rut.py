b98c5bb96f3148bea8ffb4e2ee40af8d0b57d7eb
ross <ross@892a43ee-c559-4cf5-acd1-8b7d2544a145>
2011-06-16 18:28:58 +0000

    Fixed rut config to make sure the util subpackage is packaged up.

--- projects/lib/coa/templeton/trunk/rut.py
+++ projects/lib/coa/templeton/trunk/rut.py
@@ -5,12 +5,12 @@ from elk import core, python
 
 params = dict(
               name="templeton", 
-              version="0.0.3", 
+              version="0.0.4", 
               url="http://rossfenning.co.uk/", 
               author="Ross Fenning", 
               author_email="ross.fenning@gmail.com",
               packages=['templeton', 'templeton.design', 'templeton.extract',
-                        'templeton.draw', 'templeton.resources'],
+                        'templeton.draw', 'templeton.resources', 'templeton.util'],
               depends=['python-mock', 'python-unit', 'python-imaging']
     )
 
