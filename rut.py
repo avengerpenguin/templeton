0e5756d3b77050b42921e349730883e262bc198b
ross <ross@892a43ee-c559-4cf5-acd1-8b7d2544a145>
2011-06-20 13:39:29 +0000

    Text is now curved and pasted with transparency honoured.

--- rut.py
+++ rut.py
@@ -4,10 +4,10 @@ import elk
 from elk import core, python
 
 params = dict(
-              name="templeton", 
-              version="0.2.1", 
-              url="http://rossfenning.co.uk/", 
-              author="Ross Fenning", 
+              name="templeton",
+              version="0.2.2",
+              url="http://rossfenning.co.uk/",
+              author="Ross Fenning",
               author_email="ross.fenning@gmail.com",
               packages=['templeton', 'templeton.design', 'templeton.extract',
                         'templeton.draw', 'templeton.util'],
