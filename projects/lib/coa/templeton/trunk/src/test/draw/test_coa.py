da03ad2f03632e23a23cc80c03e6a6b873f1120d
ross <ross@892a43ee-c559-4cf5-acd1-8b7d2544a145>
2011-06-20 00:09:22 +0000

    Curved text is pasted onto Coat of Arms, but is writing transparent parts of the motto layer over opaque pixels in the master image.

--- projects/lib/coa/templeton/trunk/src/test/draw/test_coa.py
+++ projects/lib/coa/templeton/trunk/src/test/draw/test_coa.py
@@ -10,7 +10,7 @@ class CoatOfArmsIllustratorTest(unittest.TestCase):
         pass
 
     def testIllustrate(self):
-        profile = {'family_name': 'Fenning'}
+        profile = {'family_name': 'Spem Successus Alit'}
         designer = CoatOfArmsDesigner()
         design = designer.design(profile)
         coai = CoatOfArmsIllustrator()
