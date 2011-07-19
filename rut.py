ccf80fcc183aa36197468f1ad52ca25fc1b7ca61
ross <ross@892a43ee-c559-4cf5-acd1-8b7d2544a145>
2011-07-19 21:19:52 +0000

    Changed motto to be a Latin-looking phrase seeded on family name rather than literally being the family name.

--- rut.py
+++ rut.py
@@ -5,14 +5,15 @@ from elk import core, python
 
 params = dict(
               name="templeton",
-              version="0.2.4",
+              version="0.3.0",
               url="http://rossfenning.co.uk/",
               author="Ross Fenning",
               author_email="ross.fenning@gmail.com",
               packages=['templeton', 'templeton.design', 'templeton.extract',
                         'templeton.draw', 'templeton.util'],
               depends=['python-mock', 'python-unit', 'python-imaging',
-                       'imagemagick', 'ttf-georgewilliams']
+                       'imagemagick', 'ttf-georgewilliams',
+                       'lorem-ipsum-generator']
     )
 
 if __name__ == "__main__":
--- src/main/templeton/design/motto.py
+++ src/main/templeton/design/motto.py
@@ -4,12 +4,21 @@ Classes for designing a motto portion of a Coat of Arms.
 
 from templeton.design.core import BaseDesigner
 
+import random, lipsum
+
 class MottoDesigner(BaseDesigner):
     """
     Designs the text of a motto. Currently uses the family name of the profile.
     """
     def design(self, profile):
         if profile.has_key('family_name'):
-            return profile['family_name']
+            family_name = profile['family_name']
+
+            random.seed(family_name)
+            lipsum_generator = lipsum.Generator()
+            lipsum_generator.sentence_mean = 3
+            lipsum_generator.sentence_sigma = 1
+            
+            return lipsum_generator.generate_sentence()
         else:
             return None
